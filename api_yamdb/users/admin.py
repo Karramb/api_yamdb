419766de14eaee579961045968d9ae92191cb4d5
Анатолий Пономарев <ap.vecna@gmail.com>
2024-11-18 20:45:57 +0300

    done

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -6,5 +6,5 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['email', 'username', 'bio', 'role']
-    list_display_links = ['email', 'username',]
+    list_display_links = ['email', 'username']
     list_editable = ['bio', 'role']
