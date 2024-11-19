4abe3ec687f49932722542bb8bc7e4f0cf77b27c
Karramb <ap.vecna@gmail.com>
2024-11-19 18:06:46 +0300

    Update admin-users

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import CustomUser
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(BaseUserAdmin):
     list_display = ['email', 'username', 'bio', 'role']
-    list_display_links = ['email', 'username']
+    list_display_links = ['email', 'username',]
     list_editable = ['bio', 'role']
